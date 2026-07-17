4ad51ae0dfd2a042e3a27ea21ed7720f63294c89
Abdul Salam <abdul@mercurie.ng>
2026-07-17 11:42:45 -0400

    initial site deploy

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCzUg87_X7J_Yi5rjsZCcyZdeZAQOa6ZPQ",
+  authDomain: "okfgo-2600d.firebaseapp.com",
+  projectId: "okfgo-2600d",
+  storageBucket: "okfgo-2600d.firebasestorage.app",
+  messagingSenderId: "635801848750",
+  appId: "1:635801848750:web:26da147d06b43b561bd63f",
+  measurementId: "G-K6KQ76W02V"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 // ==========================================
 // OKF-go CLI Marketing Site JavaScript Logic
 // Handles Terminal, Graph BFS, Parser, and Copy
